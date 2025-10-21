bd81ce5a0921cdf8a9090144cb7c6abd69324de7
chan8632 <duscks8632@gmail.com>
2025-10-21 21:58:21 +0900

    2-6 채점 style: 승패 결과에 따른 테두리 및 텍스트 색상 적용
    주요 변경 내용:
    
    App.css: .win, .lose, .tie CSS 클래스를 추가하여 승패 결과에 따라 다른 테두리/글자색(초록, 빨강, 검정)을 정의했습니다.
    
    Box.jsx: borderColor prop으로 받은 값(win, lose, tie)을 box div의 클래스명으로 동적으로 할당하여, 해당 CSS 스타일이 적용되도록 수정했습니다.

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,11 +1,8 @@
 const Box = ({ title, item, result, borderColor }) => {
   return (
-    <div className="box">
+    <div className={`box ${borderColor}`}>
       <h1>{title}</h1>
-      <img
-        className="item-img"
-        src={item && item.img}
-      />
+      <img className="item-img" src={item && item.img} />
       <h2>{result}</h2>
     </div>
   );
