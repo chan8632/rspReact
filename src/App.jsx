5a260b4295e9ce5cc21d32d4580113f68502c499
chan8632 <duscks8632@gmail.com>
2025-10-21 12:44:37 +0900

    2-3 내가 한거 feat: 유저 선택 기능 및 UI 반영
    가위바위보 버튼 클릭 시,선택한 이미지가 Box에 표시되도록 useState와 onClick 이벤트를 구현합니다.
    
    - choice 객체 (가위, 바위, 보 이미지 소스) 정의
    
    - userSelect상태를item prop으로 Box 컴포넌트에 전달
    
    - .item-img 높이(height) 300px 스타일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import Box from "./components/Box";
 import "./App.css";
-
+import { useState } from "react";
+const choice = {
+  rock: {
+    name: "rock",
+    src: "https://img.freepik.com/free-psd/grey-boulder-rock-isolated-transparent-background_632498-25568.jpg?semt=ais_hybrid&w=740&q=80",
+  },
+  scissors: {
+    name: "scissors",
+    src: "https://i.namu.wiki/i/PGp3JnsDa9eaMKBC1OwnSU4M0vLE0d_40ehrl0aUYum98U6tg0Nnl8W6_c0bQk2Bp9mQCMTe7eQt32pszxoQGw.webp",
+  },
+  paper: {
+    name: "paper",
+    src: "https://img.freepik.com/free-photo/white-paper-texture_1194-2301.jpg?semt=ais_hybrid&w=740&q=80",
+  },
+};
 function App() {
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice].src);
+  };
+  const FIRSTIMG =
+    "https://agreatdream.com/wp-content/uploads/2012/04/question-mark.jpg";
+  const [userSelect, setUserSelect] = useState(FIRSTIMG);
   return (
     <div>
       <div className="main">
-        <Box title={"You"} />
-        <Box title={"Computer"} />
+        <Box title={"You"} item={userSelect} />
+        <Box title={"Computer"} item={userSelect} />
       </div>
       <div className="main">
-        <button>가위</button>
-        <button>바위</button>
-        <button>보</button>
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
       </div>
     </div>
   );
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,10 +1,10 @@
-const Box = ({ title }) => {
+const Box = ({ title, item }) => {
   return (
     <div className="box">
       <h1>{title}</h1>
       <img
         className="item-img"
-        src="https://i.namu.wiki/i/PGp3JnsDa9eaMKBC1OwnSU4M0vLE0d_40ehrl0aUYum98U6tg0Nnl8W6_c0bQk2Bp9mQCMTe7eQt32pszxoQGw.webp"
+        src={item}
       />
       <h2>Win</h2>
     </div>
